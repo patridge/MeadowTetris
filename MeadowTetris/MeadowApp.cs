4589550e66d99832d7e5c45c6b168ee490ce3783
Adam Patridge <patridge@gmail.com>
2021-07-25 18:32:31 -0600

    Trying to switch to event-based triggers
    Not having any luck getting events to fire at all.

--- MeadowTetris/MeadowApp.cs
+++ MeadowTetris/MeadowApp.cs
@@ -6,6 +6,7 @@ using Meadow;
 using Meadow.Devices;
 using Meadow.Foundation.Displays;
 using Meadow.Foundation.Graphics;
+using Meadow.Foundation.Sensors.Buttons;
 using Meadow.Hardware;
 using Tetris;
 using static Tetris.TetrisGame;
@@ -16,10 +17,14 @@ namespace MeadowTetris
     {
         Max7219 display;
         GraphicsLibrary graphics;
-        IDigitalInputPort portLeft;
-        IDigitalInputPort portUp;
-        IDigitalInputPort portRight;
-        IDigitalInputPort portDown;
+        //IDigitalInputPort portLeft;
+        PushButton buttonLeft;
+        //IDigitalInputPort portUp;
+        PushButton buttonUp;
+        //IDigitalInputPort portRight;
+        PushButton buttonRight;
+        //IDigitalInputPort portDown;
+        PushButton buttonDown;
         TetrisGame game = new TetrisGame(8, 24);
         GameData gameData;
 
@@ -32,6 +37,7 @@ namespace MeadowTetris
                 await Initialize();
             }).ContinueWith((_) =>
             {
+                AssignButtonHandlers();
                 StartGameLoop();
             });
         }
@@ -54,12 +60,82 @@ namespace MeadowTetris
             graphics = new GraphicsLibrary(display);
             graphics.CurrentFont = new Font4x8();
             graphics.Rotation = GraphicsLibrary.RotationType._180Degrees;
-            portLeft = Device.CreateDigitalInputPort(Device.Pins.D12);
-            portUp = Device.CreateDigitalInputPort(Device.Pins.D13);
-            portRight = Device.CreateDigitalInputPort(Device.Pins.D07);
-            portDown = Device.CreateDigitalInputPort(Device.Pins.D11);
+            //portLeft = Device.CreateDigitalInputPort(Device.Pins.D12);
+            buttonLeft = new PushButton(Device.CreateDigitalInputPort(Device.Pins.D12));
+            //portUp = Device.CreateDigitalInputPort(Device.Pins.D13);
+            buttonUp = new PushButton(Device.CreateDigitalInputPort(Device.Pins.D13));
+            //portRight = Device.CreateDigitalInputPort(Device.Pins.D07);
+            buttonRight = new PushButton(Device.CreateDigitalInputPort(Device.Pins.D07));
+            //portDown = Device.CreateDigitalInputPort(Device.Pins.D11);
+            buttonDown = new PushButton(Device.CreateDigitalInputPort(Device.Pins.D11));
             Console.WriteLine("Done initializing...");
         }
+
+        private void AssignButtonHandlers()
+        {
+            Console.WriteLine($"Assinging button handlers");
+            //portLeft.Changed += PortLeft_Changed;
+            buttonLeft.Clicked += ButtonLeft_Clicked;
+            //portRight.Changed += PortRight_Changed;
+            buttonRight.Clicked += ButtonRight_Clicked;
+            //portUp.Changed += PortUp_Changed;
+            buttonUp.Clicked += ButtonUp_Clicked;
+            //portDown.Changed += PortDown_Changed;
+            buttonDown.Clicked += ButtonDown_Clicked;
+        }
+
+        private void ButtonLeft_Clicked(object sender, EventArgs e)
+        {
+            game.OnLeft();
+        }
+        private void ButtonRight_Clicked(object sender, EventArgs e)
+        {
+            game.OnRight();
+        }
+        private void ButtonUp_Clicked(object sender, EventArgs e)
+        {
+            game.OnRotate();
+        }
+        private void ButtonDown_Clicked(object sender, EventArgs e)
+        {
+            game.OnDown();
+        }
+
+        //private void PortRight_Changed(object sender, DigitalPortResult e)
+        //{
+        //    // TODO: File bug???
+        //    // For some reason, these port_changed events never fired. Switched to proper PushButtons next.
+        //    // This switch will also allow for removing the pull-up resistors I was using.
+        //    Console.WriteLine($"right: {e.Old?.State.ToString() ?? "{not set}"} {e.New.State}");
+        //    if (e.New.State)
+        //    {
+        //        game.OnRight();
+        //    }
+        //}
+        //private void PortLeft_Changed(object sender, DigitalPortResult e)
+        //{
+        //    Console.WriteLine($"left: {e.Old?.State.ToString() ?? "{not set}"} {e.New.State}");
+        //    if (e.New.State)
+        //    {
+        //        game.OnLeft();
+        //    }
+        //}
+        //private void PortUp_Changed(object sender, DigitalPortResult e)
+        //{
+        //    Console.WriteLine($"up: {e.Old?.State.ToString() ?? "{not set}"} {e.New.State}");
+        //    if (e.New.State)
+        //    {
+        //        game.OnRotate();
+        //    }
+        //}
+        //private void PortDown_Changed(object sender, DigitalPortResult e)
+        //{
+        //    Console.WriteLine($"down: {e.Old?.State.ToString() ?? "{not set}"} {e.New.State}");
+        //    if (e.New.State)
+        //    {
+        //        game.OnDown();
+        //    }
+        //}
 
         private async void Game_GameLost(object sender, GameLostEventArgs e)
         {
@@ -94,22 +170,22 @@ namespace MeadowTetris
                 game.OnDown(true);
             }
 
-            if (portLeft.State == true)
-            {
-                game.OnLeft();
-            }
-            else if (portRight.State == true)
-            {
-                game.OnRight();
-            }
-            else if (portUp.State == true)
-            {
-                game.OnRotate();
-            }
-            else if (portDown.State == true)
-            {
-                game.OnDown();
-            }
+            //if (portLeft.State == true)
+            //{
+            //    game.OnLeft();
+            //}
+            //else if (portRight.State == true)
+            //{
+            //    game.OnRight();
+            //}
+            //else if (portUp.State == true)
+            //{
+            //    game.OnRotate();
+            //}
+            //else if (portDown.State == true)
+            //{
+            //    game.OnDown();
+            //}
         }
 
         void DrawTetrisField()
